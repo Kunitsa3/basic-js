d782ea007ee85b0438fd55223fc2dfbbd7cb3aa1
Kunitsa Olya <o.kunitsa@itransition.com>
2022-10-12 00:39:52 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
@@ -13,9 +15,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  const letters = members.reduce((acc, curr) => {
+    return typeof curr === 'string' ? acc + curr.split(' ').join('')[0].toUpperCase() : acc
+  }, '');
+  return letters.split('').sort().join('');
 }
 
 module.exports = {
