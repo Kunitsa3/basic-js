c982b9d65ba350df8332c7364a16647e27af7152
Kunitsa Olya <o.kunitsa@itransition.com>
2022-10-13 19:10:03 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,9 +19,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  const isValid = typeof + sampleActivity !== 'number' || typeof sampleActivity !== 'string' || isNaN(sampleActivity);
+  const isAdequate = sampleActivity <= 15 && sampleActivity > 0;
+
+  if (isValid) {
+    return false
+  }
+
+
+  if (!isAdequate) {
+    return false
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
 }
 
 module.exports = {
