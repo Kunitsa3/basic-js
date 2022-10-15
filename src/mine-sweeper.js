c4745796aed463720fd93288fe9a729a47f3fb01
Kunitsa Olya <o.kunitsa@itransition.com>
2022-10-15 22:39:14 +0300

    feat: add mine-sweeper solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +25,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(m) {
+  const matrix = [...m];
+  const lines = matrix.length;
+  const columns = matrix[0].length;
+  const resultsArray = Array(lines).fill(null).map(() => Array(columns).fill(0));
+  for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (i + 1 < lines && matrix[i + 1][j]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (j + 1 < columns && matrix[i][j + 1]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (i - 1 >= 0 && matrix[i - 1][j]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (j - 1 >= 0 && matrix[i][j - 1]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (i - 1 >= 0 && j - 1 >= 0 && matrix[i - 1][j - 1]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (i + 1 < lines && j - 1 >= 0 && matrix[i + 1][j - 1]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (i + 1 < lines && j + 1 < columns && matrix[i + 1][j + 1]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+      if (i - 1 >= 0 && j + 1 < columns && matrix[i - 1][j + 1]) {
+        resultsArray[i][j] = +resultsArray[i][j] + 1
+      };
+
+    }
+  }
+  return resultsArray;
 }
 
 module.exports = {
