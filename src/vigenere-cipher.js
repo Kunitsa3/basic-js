a2f46532a35cb3bf600b59931b69f8db4154c477
Kunitsa Olya <o.kunitsa@itransition.com>
2022-10-15 18:17:44 +0300

    feat: add vignere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,15 +22,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode) {
+    this.mode = mode === false ? 'reverse' : 'direct';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    const m = message.toUpperCase();
+    const k = key.toUpperCase();
+    const maxLength = Math.max(m.length, k.length);
+    const a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let r = '';
+    let missed = 0;
+    for (let i = 0; i < maxLength; i++) { 
+      let flag;
+      let mi;
+      const mi_s = m[((i >= m.length) ? i % m.length : i)];
+      if (a.indexOf(mi_s) === -1){
+        flag = true;
+        missed += 1;
+      } else {
+        mi = a.indexOf(mi_s);
+      }
+      const ki_s = k[((i-missed >= k.length) ? (i - missed) % k.length : i-missed)];
+      const ki = a.indexOf(ki_s);
+      let c = flag ? mi_s : a[(((a.length + (mi + ki)) % a.length))];
+      r += c;
+    }
+    return this.mode === 'reverse' ? r.split('').reverse().join('').slice(0, message.length) : r.slice(0, message.length);
   }
-}
+  decrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    const m = message.toUpperCase();
+    const k = key.toUpperCase();
+    const maxLength = Math.max(m.length, k.length);
+    const a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let r = '';
+    let missed = 0;
+    for (let i = 0; i < maxLength; i++) { 
+      let flag;
+      let mi;
+      const mi_s = m[((i >= m.length) ? i % m.length : i)];
+      if (a.indexOf(mi_s) === -1){
+        flag = true;
+        missed += 1;
+      } else {
+        mi = a.indexOf(mi_s);
+      }
+      const ki_s = k[((i-missed >= k.length) ? (i - missed) % k.length : i-missed)];
+      const ki = -a.indexOf(ki_s);
+      let c = flag ? mi_s : a[(((a.length + (mi + ki)) % a.length))];
+      r += c;
+    }
+    return this.mode === 'reverse' ? r.split('').reverse().join('').slice(0, message.length) : r.slice(0, message.length);
+}}
+
 
 module.exports = {
   VigenereCipheringMachine
